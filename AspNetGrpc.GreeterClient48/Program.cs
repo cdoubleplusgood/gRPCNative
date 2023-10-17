31ef7a1d1d9327a7afb1a8abe0c56b2fe810fef9
Matthias Meyer <matthias.meyer@thermofisher.com>
2023-10-17 17:16:57 +0200

    Started with client implementation.

--- AspNetGrpc.GreeterClient48/Program.cs
+++ AspNetGrpc.GreeterClient48/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using Grpc.Net.Client;
 
 namespace AspNetGrpc.GreeterClient48
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            const int httpsPort = 7142;
+            //const int httpPort = 5290;
+            using (var channel = GrpcChannel.ForAddress($"https://localhost:{httpsPort}"))
+            {
+            }
+
+            //await UnaryAsync(channel, TimeSpan.FromSeconds(3.0));
+            //await ServerStreamAsync(channel, TimeSpan.FromSeconds(10.0));
+            //await ClientStreamAsync(channel);
+            //await DuplexStreamAsync(channel);
+
+            //await ServerStreamWithCancellationAsync(channel);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
