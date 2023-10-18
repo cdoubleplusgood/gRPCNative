aed1e8cbef5e962d1992bcc43093288d541d4f4c
Matthias Meyer <matthias.meyer@thermofisher.com>
2023-10-18 11:09:04 +0200

    Enable gRPC web on server.

--- AspNetGrpc.Greeter/Program.cs
+++ AspNetGrpc.Greeter/Program.cs
@@ -15,9 +15,14 @@ namespace AspNetGrpc.Greeter
             builder.Services.AddGrpc();
 
             var app = builder.Build();
+            app.UseRouting();
+            app.UseGrpcWeb();
 
             // Configure the HTTP request pipeline.
-            app.MapGrpcService<GreeterService>();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<GreeterService>().EnableGrpcWeb();
+            });
             app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
             app.Run();
--- AspNetGrpc.GreeterClient48/Program.cs
+++ AspNetGrpc.GreeterClient48/Program.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Core;
 using Grpc.Net.Client;
 using Grpc.Net.Client.Web;
+using AspNetGrpc.Greeter;
 
 namespace AspNetGrpc.GreeterClient48
 {
     internal class Program
     {
-        static void Main()
+        static async Task Main()
         {
             const int httpsPort = 7142;
-            //const int httpPort = 5290;
+            //const int httpPort = 5113;
             var httpHandler = new GrpcWebHandler(new HttpClientHandler());
             var channelOptions = new GrpcChannelOptions
             {
@@ -18,17 +21,36 @@ namespace AspNetGrpc.GreeterClient48
             };
             using (var channel = GrpcChannel.ForAddress($"https://localhost:{httpsPort}", channelOptions))
             {
+                await UnaryAsync(channel, TimeSpan.FromSeconds(3.0));
+                //await ServerStreamAsync(channel, TimeSpan.FromSeconds(10.0));
+                //await ClientStreamAsync(channel);
+                //await DuplexStreamAsync(channel);
+
+                //await ServerStreamWithCancellationAsync(channel);
             }
-
-            //await UnaryAsync(channel, TimeSpan.FromSeconds(3.0));
-            //await ServerStreamAsync(channel, TimeSpan.FromSeconds(10.0));
-            //await ClientStreamAsync(channel);
-            //await DuplexStreamAsync(channel);
-
-            //await ServerStreamWithCancellationAsync(channel);
 
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
+        }
+
+        static async Task UnaryAsync(GrpcChannel channel, TimeSpan deadline)
+        {
+            var client = new Greeter.Greeter.GreeterClient(channel);
+            var request = new HelloRequest
+            {
+                Name = "Peter Parker"
+            };
+            Console.WriteLine("=== SayHello ===");
+            Console.WriteLine($"Request: {request.Name}");
+            try
+            {
+                var reply = await client.SayHelloAsync(request, null, DateTime.UtcNow + deadline);
+                Console.WriteLine($"Reply: {reply.Message}");
+            }
+            catch (RpcException e) when (e.StatusCode == StatusCode.DeadlineExceeded)
+            {
+                Console.WriteLine("Timeout");
+            }
         }
     }
 }
