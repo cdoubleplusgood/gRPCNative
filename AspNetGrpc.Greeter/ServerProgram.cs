57ed1f7284ac9d2092e0470eda4283785d00c7cd
Matthias Meyer <matthias.meyer@thermofisher.com>
2023-10-18 18:31:15 +0200

    Simplified service configuration.

--- AspNetGrpc.Greeter/ServerProgram.cs
+++ AspNetGrpc.Greeter/ServerProgram.cs
@@ -19,10 +19,7 @@ namespace AspNetGrpc.Greeter
             app.UseGrpcWeb();
 
             // Configure the HTTP request pipeline.
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGrpcService<GreeterService>().EnableGrpcWeb();
-            });
+            app.MapGrpcService<GreeterService>().EnableGrpcWeb();
             app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
             app.Run();
