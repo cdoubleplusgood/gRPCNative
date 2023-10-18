23c2bbe5197b3e613bb11c834e47926290b076b3
Matthias Meyer <matthias.meyer@thermofisher.com>
2023-10-18 11:35:18 +0200

    Added test for client streaming.

--- AspNetGrpc.GreeterClient48/ClientProgram.cs
+++ AspNetGrpc.GreeterClient48/ClientProgram.cs
@@ -5,6 +5,7 @@ using Grpc.Core;
 using Grpc.Net.Client;
 using Grpc.Net.Client.Web;
 using AspNetGrpc.Greeter;
+using System.Threading;
 
 namespace AspNetGrpc.GreeterClient48
 {
@@ -23,7 +24,7 @@ namespace AspNetGrpc.GreeterClient48
             {
                 await UnaryAsync(channel, TimeSpan.FromSeconds(3.0));
                 //await ServerStreamAsync(channel, TimeSpan.FromSeconds(10.0));
-                //await ClientStreamAsync(channel);
+                await ClientStreamAsync(channel);
                 //await DuplexStreamAsync(channel);
 
                 //await ServerStreamWithCancellationAsync(channel);
@@ -52,5 +53,117 @@ namespace AspNetGrpc.GreeterClient48
                 Console.WriteLine("Timeout");
             }
         }
+
+    //static async Task ServerStreamAsync(GrpcChannel channel, TimeSpan deadline)
+    //{
+    //    var client = new Greeter.Greeter.GreeterClient(channel);
+    //    var request = new HelloRequest
+    //    {
+    //        Name = "Peter Parker"
+    //    };
+    //    Console.WriteLine("=== SayHelloAgain ===");
+    //    Console.WriteLine($"Request: {request.Name}");
+    //    try
+    //    {
+    //        var serverCall = client.SayHelloAgain(request, null, DateTime.UtcNow + deadline);
+    //        var serverStream = serverCall.ResponseStream;
+    //        await foreach (var reply in serverStream.ReadAllAsync())
+    //        {
+    //            Console.WriteLine($"Reply: {reply.Message}");
+    //        }
+    //    }
+    //    catch (RpcException e)
+    //    {
+    //        Console.WriteLine(e);
+    //    }
+    //}
+
+        static async Task ClientStreamAsync(GrpcChannel channel)
+        {
+            var client = new Greeter.Greeter.GreeterClient(channel);
+            string[] names =
+            {
+                "Peter Parker",
+                "Tony Stark",
+                "Steve Rogers",
+                "Bruce Banner"
+            };
+
+            Console.WriteLine("=== SayHelloToAll ===");
+            var clientStreamingCall = client.SayHelloToAll();
+            var clientStream = clientStreamingCall.RequestStream;
+            foreach (var name in names)
+            {
+                await Task.Delay(250);
+                Console.WriteLine($"Request: {name}");
+                await clientStream.WriteAsync(new HelloRequest { Name = name });
+            }
+            await clientStream.CompleteAsync();
+            var reply = await clientStreamingCall;
+            Console.WriteLine($"Reply: {reply.Message}");
+        }
+
+    //static async Task DuplexStreamAsync(GrpcChannel channel)
+    //{
+    //    var client = new Greeter.Greeter.GreeterClient(channel);
+    //    string[] names =
+    //    {
+    //        "Peter Parker",
+    //        "Tony Stark",
+    //        "Steve Rogers",
+    //        "Bruce Banner"
+    //    };
+
+    //    Console.WriteLine("=== SayHelloToEveryone ===");
+    //    var duplexStreamingCall = client.SayHelloToEveryone();
+    //    var clientStream = duplexStreamingCall.RequestStream;
+    //    var serverStream = duplexStreamingCall.ResponseStream;
+    //    var responseTask = Task.Run(async () =>
+    //    {
+    //        await foreach (var reply in serverStream.ReadAllAsync())
+    //        {
+    //            Console.WriteLine($"Reply: {reply.Message}");
+    //        }
+    //    });
+    //    foreach (var name in names)
+    //    {
+    //        await Task.Delay(150);
+    //        Console.WriteLine($"Request: {name}");
+    //        await clientStream.WriteAsync(new HelloRequest { Name = name });
+    //    }
+    //    await clientStream.CompleteAsync();
+    //    await responseTask;
+    //}
+
+    //static async Task ServerStreamWithCancellationAsync(GrpcChannel channel)
+    //{
+    //    var client = new Greeter.Greeter.GreeterClient(channel);
+    //    var request = new HelloRequest
+    //    {
+    //        Name = "Peter Parker"
+    //    };
+    //    Console.WriteLine("=== SayHelloAgain ===");
+    //    Console.WriteLine($"Request: {request.Name}");
+    //    try
+    //    {
+    //        CancellationTokenSource tokenSource = new();
+    //        CallOptions options = new(cancellationToken: tokenSource.Token);
+    //        var serverCall = client.SayHelloAgain(request, options);
+    //        var serverStream = serverCall.ResponseStream;
+    //        int count = 0;
+    //        await foreach (var reply in serverStream.ReadAllAsync(tokenSource.Token))
+    //        {
+    //            Console.WriteLine($"Reply: {reply.Message}");
+    //            if (++count >= 3)
+    //            {
+    //                tokenSource.Cancel();
+    //            }
+    //        }
+    //    }
+    //    catch (RpcException e) when (e.StatusCode == StatusCode.Cancelled)
+    //    {
+    //        Console.WriteLine("Cancelled");
+    //    }
+    //}
     }
 }
