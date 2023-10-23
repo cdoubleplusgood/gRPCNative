a77bad3c9f5ea701dd7a51c39a7256de50e24f83
Matthias Meyer <meyers-mailbox@t-online.de>
2023-10-23 22:14:45 +0200

    Enable gRPC on server.

--- GreetingService.Server6/ServerProgram.cs
+++ GreetingService.Server6/ServerProgram.cs
@@ -1,3 +1,5 @@
+using ProtoBuf.Grpc.Server;
+
 namespace GreetingService.Server6
 {
     public class ServerProgram
@@ -5,9 +7,13 @@ namespace GreetingService.Server6
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddCodeFirstGrpc();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapGrpcService<GreetingService>();
+            app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. " +
+                                  "To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
             app.Run();
         }
