0330022a91c215b0d873baa736dc1449b598e431
Matthias Meyer <meyers-mailbox@t-online.de>
2023-10-23 22:45:54 +0200

    First service call.

--- GreetingService.Client6/ClientProgram.cs
+++ GreetingService.Client6/ClientProgram.cs
@@ -1,9 +1,34 @@
-﻿namespace GreetingService.Client6
+﻿using GreetingService.Common;
+using Grpc.Net.Client;
+using ProtoBuf.Grpc.Client;
+
+namespace GreetingService.Client6
 {
     internal class ClientProgram
     {
-        static void Main()
+        static async Task Main()
         {
+            using (var channel = GrpcChannel.ForAddress("http://localhost:5151"))
+            {
+                var client = channel.CreateGrpcService<IGreetingService>();
+                var greetingService = channel.CreateGrpcService<IGreetingService>();
+
+                var greetingRequest = new GreetingRequest
+                {
+                    Greeting =
+                    {
+                        FirstName = "Peter",
+                        LastName = "Parker"
+                    }
+                };
+                Console.WriteLine("Greet");
+                Console.WriteLine("GreetRequest: " +
+                                  $"{greetingRequest.Greeting.FirstName} " +
+                                  $"{greetingRequest.Greeting.LastName}");
+                var response = await greetingService.Greet(greetingRequest);
+                Console.WriteLine($"GreetResponse: {response.Response}");
+            }
+
             Console.WriteLine("The client will stop now.");
             Console.ReadKey();
         }
