061c374353b426d5f7b18572fc48bc354972b225
Matthias Meyer <matthias.meyer@thermofisher.com>
2023-10-23 21:43:09 +0200

    Started with code first .Net 6 service.

---
+++ GreetingService.Server6/Program.cs
@@ -0,0 +1,15 @@
+namespace GreetingService.Server6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
