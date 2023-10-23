e7c5d4e89e6295915c59c313b83459d9f8c9d571
Matthias Meyer <meyers-mailbox@t-online.de>
2023-10-23 22:36:48 +0200

    Project for code first .Net 6 client.

---
+++ GreetingService.Client6/ClientProgram.cs
@@ -0,0 +1,10 @@
+﻿namespace GreetingService.Client6
+{
+    internal class ClientProgram
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
