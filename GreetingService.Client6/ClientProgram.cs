b64d58d4a011ac6d206e790a8ea8eff1da2b0804
Matthias Meyer <meyers-mailbox@t-online.de>
2023-10-23 22:55:49 +0200

    Streaming.

--- GreetingService.Client6/ClientProgram.cs
+++ GreetingService.Client6/ClientProgram.cs
@@ -27,10 +27,58 @@ namespace GreetingService.Client6
                                   $"{greetingRequest.Greeting.LastName}");
                 var response = await greetingService.Greet(greetingRequest);
                 Console.WriteLine($"GreetResponse: {response.Response}");
+
+                var greetOftenRequest = new GreetOftenRequest
+                {
+                    Greeting =
+                    {
+                        FirstName = "Peter",
+                        LastName = "Parker"
+                    },
+                    Count = 5
+                };
+                Console.WriteLine("GreetOften");
+                Console.WriteLine("GreetOftenRequest:" +
+                                  $"{greetOftenRequest.Greeting.FirstName} " +
+                                  $"{greetOftenRequest.Greeting.LastName} " +
+                                  $"{greetOftenRequest.Count}");
+                var streamResponse = greetingService.GreetOften(greetOftenRequest);
+                await foreach (var response2 in streamResponse)
+                {
+                    Console.WriteLine($"GreetResponse: {response2.Response}");
+                }
+
+                var greetingRequests = new List<GreetingRequest>
+                {
+                    new GreetingRequest { Greeting = { FirstName = "Peter", LastName = "Parker" } },
+                    new GreetingRequest { Greeting = { FirstName = "Tony", LastName = "Stark" } },
+                    new GreetingRequest { Greeting = { FirstName = "Steve", LastName = "Rogers" } }
+                };
+                Console.WriteLine("GreetAll");
+                var greetAllResponse =
+                    await greetingService.GreetAll(ToAsyncEnumerable(greetingRequests, async g =>
+                    {
+                        Console.WriteLine("GreetRequest: " +
+                                          $"{g.Greeting.FirstName} " +
+                                          $"{g.Greeting.LastName}");
+                        await Task.Delay(TimeSpan.FromSeconds(1));
+                    }));
+                Console.WriteLine($"GreetResponse: {greetAllResponse.Response}");
             }
 
             Console.WriteLine("The client will stop now.");
             Console.ReadKey();
+        }
+
+        static async IAsyncEnumerable<T> ToAsyncEnumerable<T>(IEnumerable<T> items, Func<T, Task> action)
+        {
+            foreach (var item in items)
+            {
+                // This is mainly to avoid the warning about the seemingly unnecessary "async",
+                // but it is required!
+                await action(item);
+                yield return item;
+            }
         }
     }
 }
