a84b551e5fd389f3437eccc33cdf28a8d9b3ecf1
Matthias Meyer <matthias.meyer@thermofisher.com>
2023-10-24 15:50:41 +0200

    Project for .Net 4.8 client.

---
+++ GreetingService.Client48/ClientProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GreetingService.Client48
+{
+    internal class ClientProgram
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
